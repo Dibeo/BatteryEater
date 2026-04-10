3ff409be0f30a2cf0a329272e761fa13b401f323
Simon <simsimland2004@gmail.com>
2026-04-10 18:47:02 +0200

    feat: add screen brigthness forced at 75%

--- app/src/main/java/git/batteryeater/ui/MainActivity.java
+++ app/src/main/java/git/batteryeater/ui/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,13 +32,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setBrightness(0.75f);
         setContentView(R.layout.activity_main);
 
         ImageView logo = findViewById(R.id.caillou_logo);
         TextView gpsText = findViewById(R.id.gps_text);
         final View container = findViewById(R.id.container);
 
-        // Initialisation des modules
         dvdEngine = new DvdEngine(container, logo, images);
         gpsManager = new GpsManager(this, (lat, lon) ->
                 gpsText.setText(String.format("Lat: %.6f\nLon: %.6f", lat, lon))
@@ -77,5 +78,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         gpsManager.stopUpdates();
+    }
+
+    private void setBrightness(float brightnessValue) {
+        WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+        layoutParams.screenBrightness = brightnessValue;
+        getWindow().setAttributes(layoutParams);
     }
 }
